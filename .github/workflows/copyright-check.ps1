6b15d9b673241f658f0793c5235e1614961d1097
pvijayakrish <pvijayakrish@nvidia.com>
2025-03-11 17:58:10 -0700

    Update the regex for c-code

--- .github/workflows/copyright-check.ps1
+++ .github/workflows/copyright-check.ps1
@@ -50,7 +50,7 @@ $global:copyright_matchers = @(
     found_missing = $false
     matches = @(
        '/*'
-       ' * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.'
+       ' * SPDX-FileCopyrightText: Copyright (c) ' + $date_key + ' NVIDIA CORPORATION & AFFILIATES. All rights reserved.'
        ' * SPDX-License-Identifier: Apache-2.0'
        ' *'
        ' * Licensed under the Apache License, Version 2.0 (the "License");'
