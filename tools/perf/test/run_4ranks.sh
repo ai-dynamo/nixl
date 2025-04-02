5b43b672ee5a814642d709946bbfa4284310f10e
Eyal Chocron <x41lakazam@gmail.com>
2025-04-02 22:06:18 +0300

    clean

--- tools/perf/test/run_4ranks.sh
+++
@@ -1,4 +0,0 @@
-srun --mpi=pmix --gpus-per-node=8 --ntasks-per-node=4 -N 1 \
-    --container-image="/mswg2/E2E/Regression_logs/squash/nixl_mpi.sqsh" \
-    --container-mounts="/hpc:/hpc,/swgwork/eshukrun/nixl:/swgwork/eshukrun/nixl" \
-    bash /swgwork/eshukrun/nixl/tools/perf/test/execute_4ranks.sh 2>&1 | tee log.txt
--- tools/perf/test/run_8N.sh
+++
@@ -1,4 +0,0 @@
-srun --mpi=pmix --gpus-per-node=8 --ntasks-per-node=8 -N 8 \
-    --container-image="/mswg2/E2E/Regression_logs/squash/nixl_mpi.sqsh" \
-    --container-mounts="/hpc:/hpc,/swgwork/eshukrun/nixl:/swgwork/eshukrun/nixl" \
-    bash /swgwork/eshukrun/nixl/tools/perf/test/execute_8N.sh 2>&1 | tee log.txt
