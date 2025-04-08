ddeb4dc101c438eddfb7be07dd759d74a177714f
Eyal Chocron <x41lakazam@gmail.com>
2025-04-08 17:12:44 +0300

    license

--- tools/perf/sequential_custom_traffic_perftest.py
+++ tools/perf/sequential_custom_traffic_perftest.py
@@ -1,4 +1,3 @@
-"""Sequential is different from multi in that every rank processes only one TP at a time, but they can process different ones"""
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,6 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""Sequential is different from multi in that every rank processes only one TP at a time, but they can process different ones"""
+
 import json
 import logging
 import time
