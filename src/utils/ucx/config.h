305ec6885790a9d299d8c61f85e4a9dedec5a3c6
Raul Akhmetshin <rakhmetshin@nvidia.com>
2025-06-12 17:02:06 +0000

    UTILS/UCX: Added ucx::Config class.
    Signed-off-by: Raul Akhmetshin <rakhmetshin@nvidia.com>

---
+++ src/utils/ucx/config.h
@@ -0,0 +1,58 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NIXL_SRC_UTILS_UCX_CONFIG_H
+#define NIXL_SRC_UTILS_UCX_CONFIG_H
+
+#include <memory>
+#include <string_view>
+
+extern "C" {
+#include <ucp/api/ucp.h>
+}
+
+namespace nixl {
+namespace ucx {
+    class Config {
+    public:
+        Config();
+
+        Config (const Config &) = delete;
+        Config &
+        operator= (const Config &) = delete;
+        Config (Config &&) = delete;
+        Config &
+        operator= (Config &&) = delete;
+
+        [[nodiscard]] ucp_config_t *
+        getUcpConfig() const noexcept {
+            return config_.get();
+        }
+
+        void
+        modify (std::string_view name, std::string_view value, bool force = false);
+
+    private:
+        [[nodiscard]] static ucp_config_t *
+        readUcpConfig();
+
+        const std::unique_ptr<ucp_config_t, void (*) (ucp_config_t *)> config_;
+    };
+} // namespace ucx
+} // namespace nixl
+
+#endif
