62b94f083a3a9bb7814b86487304ec688a2d4d98
Ovidiu Mara <ovidium@nvidia.com>
2025-09-05 13:37:03 +0200

    Fix build
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# shellcheck disable=SC1091
+. "$(dirname "$0")/../.ci/scripts/common.sh"
+
 set -e
 set -x
 set -o pipefail
