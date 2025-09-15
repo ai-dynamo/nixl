e7f86d3f5ac0ee1c4279dd053447f05ae5a7e7ca
Dmitry Tokarev <dtokarev@nvidia.com>
2025-09-15 17:45:42 -0400

    Simplify copyright-check.ps1
    support 2 line headers
    
    Signed-off-by: Dmitry Tokarev <dtokarev@nvidia.com>

--- .github/workflows/copyright-check.ps1
+++ .github/workflows/copyright-check.ps1
@@ -1,17 +1,5 @@
 # SPDX-FileCopyrightText: Copyright (c) 2022-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 set-strictmode -version latest
 
@@ -31,15 +19,6 @@ $global:copyright_matchers = @(
     matches = @(
       '# SPDX-FileCopyrightText: Copyright (c) ' + $date_key + ' NVIDIA CORPORATION & AFFILIATES. All rights reserved.'
       '# SPDX-License-Identifier: Apache-2.0'
-      '# Licensed under the Apache License, Version 2.0 (the "License");'
-      '# you may not use this file except in compliance with the License.'
-      '# You may obtain a copy of the License at'
-      '# http://www.apache.org/licenses/LICENSE-2.0'
-      '# Unless required by applicable law or agreed to in writing, software'
-      '# distributed under the License is distributed on an "AS IS" BASIS,'
-      '# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.'
-      '# See the License for the specific language governing permissions and'
-      '# limitations under the License.'
     )
     name = 'basic'
     regex = $null
@@ -52,19 +31,6 @@ $global:copyright_matchers = @(
        '/*'
        ' * SPDX-FileCopyrightText: Copyright (c) ' + $date_key + ' NVIDIA CORPORATION & AFFILIATES. All rights reserved.'
        ' * SPDX-License-Identifier: Apache-2.0'
-       ' *'
-       ' * Licensed under the Apache License, Version 2.0 (the "License");'
-       ' * you may not use this file except in compliance with the License.'
-       ' * You may obtain a copy of the License at'
-       ' *'
-       ' * http://www.apache.org/licenses/LICENSE-2.0'
-       ' *'
-       ' * Unless required by applicable law or agreed to in writing, software'
-       ' * distributed under the License is distributed on an "AS IS" BASIS,'
-       ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.'
-       ' * See the License for the specific language governing permissions and'
-       ' * limitations under the License.'
-       ' */'
     )
     name = 'c-code'
     regex = $null
@@ -77,15 +43,6 @@ $global:copyright_matchers = @(
       '"copyright": ['
       '  "SPDX-FileCopyrightText: Copyright (c) ' + $date_key + ' NVIDIA CORPORATION & AFFILIATES. All rights reserved.",'
       '  "SPDX-License-Identifier: Apache-2.0",'
-      '  "Licensed under the Apache License, Version 2.0 (the \"License\");",'
-      '  "you may not use this file except in compliance with the License.",'
-      '  "You may obtain a copy of the License at",'
-      '  "http://www.apache.org/licenses/LICENSE-2.0",'
-      '  "Unless required by applicable law or agreed to in writing, software",'
-      '  "distributed under the License is distributed on an \"AS IS\" BASIS,",'
-      '  "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.",'
-      '  "See the License for the specific language governing permissions and",'
-      '  "limitations under the License."'
       '],'
     )
     name = 'json'
@@ -99,16 +56,6 @@ $global:copyright_matchers = @(
       '<!--'
       'SPDX-FileCopyrightText: Copyright (c) ' + $date_key + ' NVIDIA CORPORATION & AFFILIATES. All rights reserved.'
       'SPDX-License-Identifier: Apache-2.0'
-      'Licensed under the Apache License, Version 2.0 (the "License");'
-      'you may not use this file except in compliance with the License.'
-      'You may obtain a copy of the License at'
-      'http://www.apache.org/licenses/LICENSE-2.0'
-      'Unless required by applicable law or agreed to in writing, software'
-      'distributed under the License is distributed on an "AS IS" BASIS,'
-      'WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.'
-      'See the License for the specific language governing permissions and'
-      'limitations under the License.'
-      '-->'
     )
     name = 'markdown'
     regex = $null
@@ -120,15 +67,6 @@ $global:copyright_matchers = @(
     matches = @(
       '// SPDX-FileCopyrightText: Copyright (c) ' + $date_key + ' NVIDIA CORPORATION & AFFILIATES. All rights reserved.'
       '// SPDX-License-Identifier: Apache-2.0'
-      '// Licensed under the Apache License, Version 2.0 (the "License");'
-      '// you may not use this file except in compliance with the License.'
-      '// You may obtain a copy of the License at'
-      '// http://www.apache.org/licenses/LICENSE-2.0'
-      '// Unless required by applicable law or agreed to in writing, software'
-      '// distributed under the License is distributed on an "AS IS" BASIS,'
-      '// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.'
-      '// See the License for the specific language governing permissions and'
-      '// limitations under the License.'
     )
     name = 'c-like'
     regex = $null
