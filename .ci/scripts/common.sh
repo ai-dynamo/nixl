9aab08a2e6a6769e0dc375b8def35b0c27f48cd3
Ovidiu Mara <ovidium@nvidia.com>
2025-09-18 12:53:13 +0200

    Revert changes to CUDA load path
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>

--- .ci/scripts/common.sh
+++ .ci/scripts/common.sh
@@ -84,7 +84,5 @@ else
     echo "==== CUDA support not found ===="
     HAS_CUDA=false
     UCX_CUDA_BUILD_ARGS=""
-    # Normal CUDA installation: load CUDA from lib64/stubs
-    # nvcr.io images: load CUDA from compat/lib.real
-    export LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs:/usr/local/cuda/compat/lib.real:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/lib64/stubs:/usr/local/cuda/compat:/usr/local/cuda/compat/lib.real:$LD_LIBRARY_PATH
 fi
