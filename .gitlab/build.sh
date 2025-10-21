6c8b2ab3602843ab1e66dda7b9e70b40356bef81
Alexey Rivkin <arivkin@nvidia.com>
2025-10-21 12:28:58 +0300

    Set CUDA_HOME in the build script
    Signed-off-by: Alexey Rivkin <arivkin@nvidia.com>

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Ensure CUDA_HOME is set (cuda-dl-base images don't set it by default)
+export CUDA_HOME=${CUDA_HOME:-/usr/local/cuda}
+
 # shellcheck disable=SC1091
 . "$(dirname "$0")/../.ci/scripts/common.sh"
 
