c35e56ed28df282c984909d6f8d23f64171204cf
Raul Akhmetshin <rakhmetshin@nvidia.com>
2025-11-05 14:56:40 +0200

    CORE: Added MSG_NOSIGNAL.
    Signed-off-by: Raul Akhmetshin <rakhmetshin@nvidia.com>

--- src/core/nixl_listener.cpp
+++ src/core/nixl_listener.cpp
@@ -106,18 +106,22 @@ sendCommMessage(int fd, const std::string& msg) {
     };
 
     for (size_t i = 0, offset = 0, sent = 0; i < iov_size;) {
-        auto bytes = send(fd, static_cast<char *>(iov[i].iov_base) + offset, iov[i].iov_len - offset, 0);
+        auto bytes = send(fd,
+                          static_cast<char *>(iov[i].iov_base) + offset,
+                          iov[i].iov_len - offset,
+                          MSG_NOSIGNAL);
         if (bytes < 0) {
             if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK) {
                 continue;
             }
 
             throw std::runtime_error(
-                    absl::StrFormat("sendCommMessage(fd=%d) %zu/%zu bytes failed, errno=%d",
-                                    fd,
-                                    sent,
-                                    size + sizeof(size),
-                                    errno));
+                absl::StrFormat("sendCommMessage(fd=%d, msg=%s) %zu/%zu bytes failed, errno=%d",
+                                fd,
+                                msg.c_str(),
+                                sent,
+                                size + sizeof(size),
+                                errno));
         }
 
         offset += bytes;
