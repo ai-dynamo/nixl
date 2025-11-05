bac2273456de8d0696b66f5dd35eb4125e71b897
Raul Akhmetshin <rakhmetshin@nvidia.com>
2025-11-05 13:06:00 +0200

    TEST/NIXL: Test CPP failures.
    Signed-off-by: Raul Akhmetshin <rakhmetshin@nvidia.com>

--- src/core/nixl_listener.cpp
+++ src/core/nixl_listener.cpp
@@ -662,6 +662,10 @@ void nixlAgentData::commWorker(nixlAgent* myAgent){
 }
 
 void nixlAgentData::enqueueCommWork(nixl_comm_req_t request){
+    if (agentShutdown) {
+        NIXL_WARN << "Agent shutting down, unable to accept new requests";
+        return;
+    }
     std::lock_guard<std::mutex> lock(commLock);
     commQueue.push_back(std::move(request));
 }
