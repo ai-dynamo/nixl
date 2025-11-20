01ba036e647070db1002cd2ff2157b4a5c5cb808
Shachar Hasson <shasson5@gmail.com>
2025-11-20 12:52:07 +0200

    TEST: clang format

--- src/plugins/ucx/ucx_backend.cpp
+++ src/plugins/ucx/ucx_backend.cpp
@@ -1104,11 +1104,8 @@ nixlUcxEngine::nixlUcxEngine(const nixlBackendInitParams &init_params)
     const auto has_config = (custom_params->find("engine_config") != custom_params->end());
     const auto engine_config = has_config ? (*custom_params)["engine_config"] : "";
 
-    uc = std::make_unique<nixlUcxContext>(devs,
-                                          init_params.enableProgTh,
-                                          num_workers,
-                                          init_params.syncMode,
-                                          engine_config);
+    uc = std::make_unique<nixlUcxContext>(
+        devs, init_params.enableProgTh, num_workers, init_params.syncMode, engine_config);
 
     for (size_t i = 0; i < num_workers; i++) {
         uws.emplace_back(std::make_unique<nixlUcxWorker>(*uc, err_handling_mode));
