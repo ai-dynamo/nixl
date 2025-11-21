835c6367f01c767542ccb03fed82cd9439492486
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2025-11-21 12:13:35 +0530

    Fix formatting
    Signed-off-by: Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>

--- src/plugins/uccl_p2p/uccl_p2p_backend.cpp
+++ src/plugins/uccl_p2p/uccl_p2p_backend.cpp
@@ -285,7 +285,7 @@ nixlUcclEngine::registerMem(const nixlBlobDesc &mem,
     out = priv;
     mem_reg_info_[mem.addr] = priv;
     NIXL_DEBUG << "Registering memory: " << mem.addr << "Device: " << mem.devId
-               << " ref_cnt: " << priv->ref_cnt << " mr_id: " << priv->mr_id;   
+               << " ref_cnt: " << priv->ref_cnt << " mr_id: " << priv->mr_id;
 
     return NIXL_SUCCESS;
 }
