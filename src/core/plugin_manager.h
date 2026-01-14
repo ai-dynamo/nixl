c540a91cd75dae2855247badff38d274777af149
Ovidiu Mara <ovidium@nvidia.com>
2026-01-14 14:35:31 +0100

    Refactor plugin manager to improve memory safety and string handling
    Signed-off-by: Ovidiu Mara <ovidium@nvidia.com>

--- src/core/plugin_manager.h
+++ src/core/plugin_manager.h
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -71,12 +71,12 @@ private:
 
 // Structure to hold static plugin info
 struct nixlBackendStaticPluginInfo {
-    const char* name;
+    std::string name;
     nixlStaticPluginCreatorFunc createFunc;
 };
 
 struct nixlTelemetryStaticPluginInfo {
-    const char *name;
+    std::string name;
     nixlTelemetryStaticPluginCreatorFunc createFunc;
 };
 
@@ -97,7 +97,7 @@ private:
 };
 
 typedef std::shared_ptr<const nixlPluginHandle> (
-    *nixlPluginLoaderFunc)(void *handle, const std::string_view &plugin_path);
+    *nixlPluginLoaderFunc)(void *handle, const std::string &plugin_path);
 
 class nixlPluginManager {
 public:
@@ -167,14 +167,14 @@ private:
     void
     registerBuiltinPlugins();
     void
-    registerBackendStaticPlugin(const std::string_view &name, nixlStaticPluginCreatorFunc creator);
+    registerBackendStaticPlugin(const std::string &name, nixlStaticPluginCreatorFunc creator);
     void
-    registerTelemetryStaticPlugin(const std::string_view &name,
+    registerTelemetryStaticPlugin(const std::string &name,
                                   nixlTelemetryStaticPluginCreatorFunc creator);
 
     // Search a directory for plugins
     void
-    discoverPluginsFromDir(const std::string_view &dirpath);
+    discoverPluginsFromDir(const std::string &dirpath);
 
     // Discover helper functions
     void
