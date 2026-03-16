c8108542320270971fae02f2718f9edf4ffefc41
Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>
2026-03-16 11:43:20 +0530

    Fix typo error
    Signed-off-by: Pravein Govindan Kannan <pravein.govindan.kannan@ibm.com>

--- src/plugins/uccl/uccl_backend.h
+++ src/plugins/uccl/uccl_backend.h
@@ -143,7 +143,7 @@ class nixlUcclBackendMD : public nixlBackendMD {
 public:
     nixlUcclBackendMD(bool isPrivate) : nixlBackendMD(isPrivate) {
         memset(fifo_item, 0, FIFO_SIZE);
-        memset(ipc_info, 0, IPC_INFO_BUF_SIZE);
+        memset(ipc_info, 0, IPC_INFO_SIZE);
     }
 
     virtual ~nixlUcclBackendMD() {}
@@ -153,7 +153,7 @@ public:
     int ref_cnt;
     uccl_mr_t mr_id; // UCCL memory region id
     char fifo_item[FIFO_SIZE];
-    char ipc_info[IPC_INFO_BUF_SIZE]; // serialized IPC transfer info for cross-process local
+    char ipc_info[IPC_INFO_SIZE]; // serialized IPC transfer info for cross-process local
     bool has_ipc = false;             // true if GPU buffer with valid IPC handle
 };
 
