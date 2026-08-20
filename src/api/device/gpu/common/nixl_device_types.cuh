4e79b727e52519ab04c9a9619a13a5e2c8dcd992
hbadihi <hbadihi@nvidia.com>
2026-08-20 13:45:06 -0700

    device_api: establish backend-neutral GPU API infrastructure

---
+++ src/api/device/gpu/common/nixl_device_types.cuh
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef NIXL_SRC_API_DEVICE_GPU_COMMON_NIXL_DEVICE_TYPES_CUH
+#define NIXL_SRC_API_DEVICE_GPU_COMMON_NIXL_DEVICE_TYPES_CUH
+
+#include <cstddef>
+#include <cstdint>
+
+#include <nixl_types.h>
+
+struct nixlGpuXferStatusH {
+    alignas(16) unsigned char storage[64] = {};
+};
+
+constexpr size_t nixl_gpu_xfer_status_payload_size = 64;
+
+static_assert(nixl_gpu_xfer_status_payload_size == sizeof(nixlGpuXferStatusH));
+
+enum class nixl_gpu_level_t : uint64_t { THREAD = 0, WARP = 1, BLOCK = 2, GRID = 3 };
+
+namespace nixl_gpu_flags {
+constexpr uint64_t defer = 1;
+} // namespace nixl_gpu_flags
+
+struct nixlMemViewElem {
+    nixlMemViewH mvh;
+    size_t index; /**< Index in the memory view */
+    size_t offset; /**< Offset within the buffer */
+};
+
+#endif // NIXL_SRC_API_DEVICE_GPU_COMMON_NIXL_DEVICE_TYPES_CUH
