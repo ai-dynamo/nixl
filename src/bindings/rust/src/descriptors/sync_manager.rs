a7ba071d54e7c91485eeb0a6f2a38142d43f1530
Mason Pitt <bradley.b.pitt@gmail.com>
2026-08-27 12:01:53 +0800

    fix: update copyright years for modified Rust descriptors

--- src/bindings/rust/src/descriptors/sync_manager.rs
+++ src/bindings/rust/src/descriptors/sync_manager.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 //! Synchronization manager for frontend-backend data consistency
